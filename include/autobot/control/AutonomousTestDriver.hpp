c7c3d3d947aeedcda35704a3d794c8a2cc021b32
euphoriafan755-lgtm <euphoriafan755@gmail.com>
2026-09-15 20:37:10 -0300

    Add Cube-only autonomous test driver interface

---
+++ include/autobot/control/AutonomousTestDriver.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "autobot/control/InputController.hpp"
+#include "autobot/core/GameSnapshot.hpp"
+#include "autobot/world/CollisionWorld.hpp"
+
+#include <cstddef>
+#include <string>
+
+namespace autobot::control {
+
+struct AutonomousDecision {
+    bool enabled = false;
+    bool active = false;
+    InputOwnership ownership = InputOwnership::User;
+    InputAction action = InputAction::NoPress;
+    std::string reason = "AUTOBOT DISABLED";
+    std::size_t targetPrimitiveIndex = world::kInvalidPrimitiveIndex;
+    int targetObjectID = 0;
+    double targetDistance = 0.0;
+};
+
+class AutonomousTestDriver final {
+public:
+    [[nodiscard]] AutonomousDecision decide(
+        core::GameSnapshot const& snapshot,
+        world::CollisionWorld const& collisionWorld,
+        world::CollisionQueryResult const& query,
+        bool enabled,
+        bool botHolding
+    ) const;
+};
+
+[[nodiscard]] char const* toString(InputOwnership value);
+[[nodiscard]] char const* toString(InputAction value);
+
+} // namespace autobot::control
