d22af582efc9f81bbb8d79e2b02391b0f9763c7b
euphoriafan755-lgtm <euphoriafan755@gmail.com>
2026-09-15 21:47:06 -0300

    General solver: add mode action candidate generator

---
+++ include/autobot/solver/ActionGenerator.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "autobot/core/GameSnapshot.hpp"
+#include "autobot/solver/SolverTypes.hpp"
+
+#include <cstddef>
+#include <vector>
+
+namespace autobot::solver {
+
+class ModeActionGenerator final {
+public:
+    [[nodiscard]] std::vector<ActionCandidate> generate(
+        core::GameSnapshot const& snapshot,
+        std::size_t horizonTicks
+    ) const;
+
+private:
+    static ActionCandidate constant(char const* label, bool hold, std::size_t ticks);
+    static ActionCandidate delayedTap(char const* label, std::size_t delay, std::size_t horizon);
+    static ActionCandidate holdThenRelease(
+        char const* label,
+        std::size_t holdTicks,
+        std::size_t horizon
+    );
+    static ActionCandidate releaseThenHold(
+        char const* label,
+        std::size_t releaseTicks,
+        std::size_t horizon
+    );
+};
+
+} // namespace autobot::solver
