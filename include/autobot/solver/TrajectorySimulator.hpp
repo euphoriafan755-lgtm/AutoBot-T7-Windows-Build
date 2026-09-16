0c8d29f6831cf4a9434bfe377c423ea21e70ba48
euphoriafan755-lgtm <euphoriafan755@gmail.com>
2026-09-15 21:47:18 -0300

    General solver: add multimode trajectory simulator

---
+++ include/autobot/solver/TrajectorySimulator.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "autobot/core/GameSnapshot.hpp"
+#include "autobot/solver/LocalWorldView.hpp"
+#include "autobot/solver/PhysicsModels.hpp"
+#include "autobot/solver/PortalTransition.hpp"
+#include "autobot/solver/SolverTypes.hpp"
+#include "autobot/world/CollisionWorld.hpp"
+
+#include <cstddef>
+
+namespace autobot::solver {
+
+class TrajectorySimulator final {
+public:
+    [[nodiscard]] TrajectoryResult simulate(
+        core::GameSnapshot const& snapshot,
+        world::CollisionWorld const& collisionWorld,
+        LocalWorldView const& localWorld,
+        PhysicsValidationHarness const& validation,
+        ActionCandidate const& candidate,
+        std::size_t horizonTicks
+    ) const;
+
+private:
+    ModePhysicsRegistry m_registry{};
+};
+
+} // namespace autobot::solver
